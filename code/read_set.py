f58102126259f49b7e4168d313a07f8def7e72e8
KwatME <kwat.me@icloud.com>
2021-08-28 15:06:00 -0600

    Ran all

--- code/read_set.py
+++ code/read_set.py
@@ -1,15 +1,7 @@
-from os.path import splitext
-
 from kwat.gmt import read
 from kwat.path import list_directory
 
 
 def read_set():
 
-    return read(
-        [
-            pa
-            for pa in list_directory("../input/set/")
-            if splitext(pa)[1] in [".gmt", ".tsv"]
-        ]
-    )
+    return read(list_directory("../input/set/", ke_=(r".gmt$", r".tsv$")))
